6161ec17ad45482cba29d8446bb18f2e3204a067
Alec <alecvbnm@outlook.com>
2025-11-09 15:01:25 +0800

    init

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }: {
+  imports = [
+    ../bar.nix
+    ../clipboard.nix
+    ../menu.nix
+    ../notification.nix
+
+    # Binding Related
+    ../browser.nix
+    ../torrent.nix
+    ../terminal.nix
+    ../lock.nix
+    ../wallpaper.nix
+  ];
+
+  home.packages = with pkgs; [ hyprshot xdg-launch networkmanagerapplet ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enableXdgAutostart = true;
+    extraConfig = builtins.readFile ./hypr/hyprland.conf;
+    plugins = with pkgs.hyprlandPlugins; [ hyprexpo ];
+  };
+
+  xdg.configFile."hypr/hyprland.conf.d".source =
+    config.lib.file.mkOutOfStoreSymlink ./hypr/hyprland.conf.d;
+
+  mine.de.idle.display = status:
+    "hyprctl dispatch dpms ${if status then "on" else "off"}";
+
+  # Polkit Agent
+  services.hyprpolkitagent.enable = true;
+}
